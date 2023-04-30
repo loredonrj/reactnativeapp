3bc4ba2b784393237b2230bdb3fde12c4e8c363c
loredonrj <loredonrj@users.noreply.github.com>
2023-04-30 20:57:05 +0000

    import RestaurantScreen ie add "Restaurant" screen

--- reactnativeapp/navigation.js
+++ reactnativeapp/navigation.js
@@ -6,12 +6,14 @@ const Stack = createNativeStackNavigator();
 import { View, Text } from 'react-native'
 import React from 'react'
 import HomeScreen from './HomeScreen';
+import RestaurantScreen from './RestaurantScreen';
 
 export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
