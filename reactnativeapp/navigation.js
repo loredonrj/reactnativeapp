6c50b2f1139ffc7d02c229af3b3139bf3e03c3fd
loredonrj <loredonrj@users.noreply.github.com>
2023-04-30 20:54:20 +0000

    import HomeScreen Component into navigation.js

--- reactnativeapp/navigation.js
+++ reactnativeapp/navigation.js
@@ -5,6 +5,7 @@ const Stack = createNativeStackNavigator();
 
 import { View, Text } from 'react-native'
 import React from 'react'
+import HomeScreen from './HomeScreen';
 
 export default function Navigation() {
   return (
