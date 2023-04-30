9fc452cf79b7c8c63c586c5f6bc5b8de29a926e8
loredonrj <loredonrj@users.noreply.github.com>
2023-04-30 22:33:54 +0000

    mod nv container to hide headers

--- reactnativeapp/navigation.js
+++ reactnativeapp/navigation.js
@@ -11,7 +11,7 @@ import RestaurantScreen from './screens/RestaurantScreen';
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
